1385d6766f2e894d0c097a05ec702821c74136f3
lromang <luis.roangarci@gmail.com>
2017-06-16 12:29:43 -0500

    code py

--- src/code.py
+++ src/code.py
@@ -1,10 +1,25 @@
+##################################################
+## Chapter 2
+## ----------------------------------------
+## Sjardin, Bastiaan, Luca Massaron, and
+## Alberto Boschetti.
+## Large Scale Machine Learning with Python.
+## Packt Publishing Ltd, 2016.
+##################################################
+
+
+## ------------------------------
 ## Libraries
-import pandas
-import scipy
-import numpy
-import sklearn
-import tensorflow as tf
+## ------------------------------
+import os, csv
+import pandas as pd
 
+## ------------------------------
 ## Read in data
-
-##
+## ------------------------------
+source     = '../data/bikesharing/hour.csv'
+CHUNK_SIZE = 1000
+with open(source, 'rb') as R:
+    iterator = pd.read_csv(R, chunksize = CHUNK_SIZE)
+    for n, data_chunk in enumerate(iterator):
+        print ('Size of uploaded chunk: %i instances, %i features' % (data_chunk.shape))
